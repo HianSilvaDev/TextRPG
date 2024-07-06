7944b960bd28c93c97d97472ed69fa01e70e81dc
HianSilvaDev <hianvitorsantossilva@gmail.com>
2024-07-06 04:38:52 +0000

    Navigation System
    Made the navigation system based in colors and cordinates.

--- public/js/world.js
+++ public/js/world.js
@@ -1,14 +1,57 @@
 const coordinates = document.getElementById("coordinates")
+const txt = document.querySelector(".txt")
 
 coordinates.textContent = '--,--'
 
-let coordinatesX = 0;
-let coordinatesY = 0;
+let cordinatesX = 550;
+let cordinatesY = 100;
+let currentRegion;
 
-function newCoordinates(newX,newY)
-{
-  coordinatesX = coordinatesX + newX
-  coordinatesY = coordinatesY + newY
+const canvas = document.getElementById('mapCanvas');
+const ctx = canvas.getContext('2d');
+const img = new Image();
+img.src = '/public/assets/img/2DMAP.png';
 
-  coordinates.textContent = `(${coordinatesX},${coordinatesY})`
+img.onload = function() {
+    canvas.width = img.width;
+    canvas.height = img.height;
+    ctx.drawImage(img, 0, 0);
+};
+
+const colorToLocality = {
+    "rgb(51, 119, 85)": "Floresta",
+    "rgb(85, 153, 68)": "Clareira",
+    "rgb(182, 45, 45)": "Cidade",
+    "rgb(204, 255, 51)": "Vilarejo",
+    "rgb(255, 255, 255)": "Topo da Montanha",
+    "rgb(153, 170, 119)": "Pé da Montanha",
+    "rgb(51, 102, 153)": "Lago ou rio"
+};
+
+function getPixelColor(x, y) {
+    const imageData = ctx.getImageData(x, y, 1, 1).data;
+    return `rgb(${imageData[0]}, ${imageData[1]}, ${imageData[2]})`;
 }
+
+function getLocalityNameByColor(x, y) {
+    const color = getPixelColor(x, y);
+    return colorToLocality[color] || currentRegion;
+}
+
+img.onload = function() {
+    canvas.width = img.width;
+    canvas.height = img.height;
+    ctx.drawImage(img, 0, 0);
+};
+
+function newCordinates(newx, newy){
+    cordinatesX = cordinatesX + newx;
+    cordinatesY = cordinatesY + newy;
+    currentRegion = getLocalityNameByColor(cordinatesX, cordinatesY);
+    update();
+};
+
+function update(){
+    coordinates.textContent = cordinatesX + ", " + cordinatesY;
+    txt.textContent = currentRegion;
+};
