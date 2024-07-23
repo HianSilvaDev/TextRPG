632e4ad958d83787b3009d45eb039e81f9ec77b6
HianSilvaDev <hianvitorsantossilva@gmail.com>
2024-07-23 12:15:12 -0300

    Update HomeControler.js

--- controllers/HomeControler.js
+++ controllers/HomeControler.js
@@ -1,25 +1,15 @@
 const path = require('path');
 
 const home = (req, res) => {
-  if(!req.session.user){
-    return res.redirect('/')
-  }
   return res.sendFile(path.resolve(path.join(__dirname, '../public/pages/home.html')));
 }
 
 const index = (req, res) => {
-  if(req.session.user){
-    return res.redirect('/home')
-  }
   return res.sendFile(path.resolve(path.join(__dirname, '../public/pages/index.html')))
 }
 
 const world = (req, res) => {
-  if(req.session.user){
-res.sendFile(path.resolve(path.join(__dirname, '../public/pages/world.html')))
-  }else{
-    res.redirect('/')
-  }
+  res.sendFile(path.resolve(path.join(__dirname, '../public/pages/world.html')))
 }
 
 const admin = (req, res) => {
