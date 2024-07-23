b57ae36ee1538855ddb790c41f375dbde756765a
HianSilvaDev <hianvitorsantossilva@gmail.com>
2024-07-23 12:12:25 -0300

    Update playerController.js

--- controllers/playerController.js
+++ controllers/playerController.js
@@ -1,46 +1,33 @@
-const Player = require("../models/PlayerModel");
-
-const create = (req, res) => {
-  const {classe, name} = req.body;
-  const id = req.session.user.id;
-  const newPlayer = new Player(id, name, classe);
-  try{
-    newPlayer.save();
-    res.status(201).json({ message: "Pesornagens criado com sucesso!" });
-  }catch(err){
-    console.log(err)
-    res.status(500).json({ error: "Erro ao criar personagens!" });
-  }
-}
+const PlayerModel = require("../models/playerModel");
 
 const get = async (req, res) => {
-  const id = req.session.user.id;
-  try{
-    const player = await Player.getById(id);
-    if(player){
+  const { id } = req.body;
+  console.log(id);
+  try {
+    const player = await PlayerModel.getById(id);
+    if (player) {
       res.status(200).json(player);
-    }else{
+    } else {
       res.status(404).json({ error: "Personagem não encontrado!" });
     }
-  }catch(err){
-    console.log(err)
+  } catch (err) {
+    console.log(err);
     res.status(500).json({ error: "Erro ao buscar o Personagem!" });
   }
-}
+};
 
-const update = (req, res) => {
-  const player = req.body
-  try{
-    Player.update(player);
-    res.status(200).json({ message: "Personagem atualizado com sucesso!" })
-  }catch(err){
-    console.log(err)
-    res.status(500).json({ error: "Erro ao atualizar o Personagem!" })
+const update = async (req, res) => {
+  const player = req.body;
+  try {
+    await PlayerModel.update(player);
+    res.status(200).json({ message: "Personagem atualizado com sucesso!" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Erro ao atualizar o Personagem!" });
   }
-}
+};
 
 module.exports = {
-  create,
   update,
-  get
-}
+  get,
+};
