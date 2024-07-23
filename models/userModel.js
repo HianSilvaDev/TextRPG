b13473d6580bca945680efb3a3ff4705b9e7eb43
HianSilvaDev <hianvitorsantossilva@gmail.com>
2024-07-23 12:18:16 -0300

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -1,103 +1,51 @@
-const fs = require("fs");
-const path = require("path");
-const uuid = require("uuid");
+const prisma = require("./prismaClient");
 
-const filePath = path.join(__dirname, "../data/users.json");
-
-class User {
-  constructor(name, password, email) {
-    this.id = uuid.v4();
-    this.name = name;
-    this.email = email;
-    this.password = password;
-  }
-
-  static getAll() {
-    return new Promise((resolve, reject) => {
-      fs.readFile(filePath, "utf8", (err, data) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(JSON.parse(data));
-        }
-      });
+const createUser = async (name, email, hashedPassword, classe) => {
+  try {
+    const newUser = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+        player: {
+          create: {
+            name: name,
+            class: classe.class,
+            hp: classe.hp,
+            mp: classe.mp,
+            strength: classe.strength,
+            defense: classe.defense,
+            dexterity: classe.dexterity,
+            resistance: classe.resistence,
+            intelligence: classe.intelligence,
+            luck: classe.luck,
+          },
+        },
+      },
     });
+    console.log("User created in database:", newUser);
+    return newUser;
+  } catch (err) {
+    console.error("Error creating user:", err);
+    throw err;
   }
+};
 
-  static saveAll(users) {
-    return new Promise(async (resolve, reject) => {
-      fs.writeFile(filePath, JSON.stringify(users), (err) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve();
-        }
-      });
+async function getUserByEmail(emailOrName) {
+  console.log(emailOrName);
+  try {
+    const user = await prisma.user.findFirst({
+      where: {
+        OR: [{ name: emailOrName }, { email: emailOrName }],
+      },
     });
-  }
-
-  save() {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const users = await User.getAll();
-        users.push(this);
-        await User.saveAll(users);
-        resolve();
-      } catch (err) {
-        reject(err);
-      }
-    });
-  }
-
-  static getById(id) {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const users = await User.getAll();
-        const user = users.find((u) => u.id === id);
-        resolve(user);
-      } catch (err) {
-        reject(err);
-      }
-    });
-  }
-
-  static getByEmail(email) {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const users = await User.getAll();
-        const user = users.find((u) => u.email === email);
-        resolve(user);
-      } catch (err) {
-        reject(err);
-      }
-    });
-  }
-
-  static getByName(name) {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const users = await User.getAll();
-        const user = users.find((u) => u.name === name);
-        resolve(user);
-      } catch (err) {
-        reject(err);
-      }
-    });
-  }
-
-  static delete(id) {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const users = await User.getAll();
-        const index = users.findIndex((u) => u.id === id);
-        users.splice(index, 1);
-        await User.saveAll(users);
-        resolve();
-      } catch (err) {
-        reject(err);
-      }
-    });
+    return user;
+  } catch (err) {
+    throw err;
   }
 }
 
-module.exports = User;
+module.exports = {
+  createUser,
+  getUserByEmail,
+};
