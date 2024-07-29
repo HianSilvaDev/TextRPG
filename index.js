df5e816f08fa6f0b340c17512dd4611614fd9cd7
Hian Silva <hianvitorsantossilva@gmail.com>
2024-07-29 11:39:54 +0000

    delete old files

--- index.js
+++ index.js
@@ -1,39 +1,27 @@
-const express = require('express');
-const session = require('express-session')
+const express = require("express");
 
 const app = express();
 
 app.use(express.json());
 
-app.use(session({
-  secret: 'keyboardcat',
-  resave: false,
-  saveUninitialized: true
-}))
-
 app.use("/public", express.static(__dirname + "/public"));
 
 // Importando Rotas:
 const index = require("./router/indexRoutes.js");
-app.use('/', index)
+app.use("/", index);
 
 const user = require("./router/userRoutes.js");
-app.use('/user', user)
+app.use("/user", user);
 
 const player = require("./router/playerRoutes.js");
-app.use('/player', player)
+app.use("/player", player);
 
 const enemy = require("./router/enemyRoutes.js");
-app.use('/enemy', enemy)
-
-const cord = require("./router/cordRoutes.js");
-app.use('/map', cord)
+app.use("/enemy", enemy);
 
 const region = require("./router/regionRoutes.js");
-app.use('/region', region)
+app.use("/region", region);
 
 app.listen(3000, () => {
-  console.log('server started');
-})
-
-// forgot the password
+  console.log("server started");
+});
