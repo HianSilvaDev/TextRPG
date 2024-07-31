c34511cc1d93247c937b057ff3435fefca1007db
HianSilvaDev <hianvitorsantossilva@gmail.com>
2024-07-30 22:35:57 -0300

    Update regionModel.js

--- models/regionModel.js
+++ models/regionModel.js
@@ -2,7 +2,7 @@ const prisma = require("./prismaClient");
 
 async function getByName(regionName) {
   try {
-    const regionWithEnemies = await prisma.region.findUnique({
+    const region = await prisma.region.findUnique({
       where: {
         name: regionName,
       },
@@ -29,31 +29,7 @@ async function getByName(regionName) {
         },
       },
     });
-    if (regionWithEnemies) {
-      const formattedRegion = {
-        name: region.name,
-        desc: region.desc,
-        image_urls: region.image_urls,
-        enemies: region.Spawnpoint.map((spawnpoint) => ({
-          enemy_id: spawnpoint.Enemy.enemy_id,
-          name: spawnpoint.Enemy.name,
-          desc: spawnpoint.Enemy.desc,
-          level: spawnpoint.Enemy.level,
-          spawnrate: spawnpoint.Enemy.spawnrate,
-          hp: spawnpoint.Enemy.hp,
-          mp: spawnpoint.Enemy.mp,
-          strength: spawnpoint.Enemy.strength,
-          defense: spawnpoint.Enemy.defense,
-          dexterity: spawnpoint.Enemy.dexterity,
-          resistance: spawnpoint.Enemy.resistance,
-          intelligence: spawnpoint.Enemy.intelligence,
-          luck: spawnpoint.Enemy.luck,
-          Loot: spawnpoint.Enemy.Loot,
-          EnemySkills: spawnpoint.Enemy.EnemySkills,
-        })),
-      };
-      console.log(formattedRegion);
-    }
+    return region
   } catch (err) {
     console.log(err);
   }
