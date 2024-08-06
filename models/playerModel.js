0d85e5b9645da13e5f7fdd3df0f1f4e45412cf6a
HianSilvaDev <hianvitorsantossilva@gmail.com>
2024-08-05 23:05:58 -0300

    Update playerModel.js

--- models/playerModel.js
+++ models/playerModel.js
@@ -1,86 +1,17 @@
-const fs = require('fs');
-const path = require('path');
+const prisma = require("./prismaClient.js");
 
-const filePath = path.join(__dirname, '../data/player.json');
-
-class Player{
-  constructor(id, name, classe = {}){
-    this.id = id;
-    this.name = name;
-    this.level = 1;
-    this.wallet = 0;
-    this.applyClass(classe)
-  }
-  
-  applyClass(classe) {
-    for(let key in classe){
-      this[key] = classe[key];
-    }
-  }
-
-  static getAll() {
-    return new Promise((resolve, reject) => {
-      fs.readFile(filePath, 'utf8', (err, data) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(JSON.parse(data));
-        }
-      })
-    })
-  }
-
-  static saveAll(users){
-    return new Promise(async (resolve, reject) => {
-      fs.writeFile(filePath, JSON.stringify(users), (err) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve();
-        }
-      })
-    })
-  }
-
-  save(){
-    return new Promise(async (resolve, reject) => {
-      try{
-        const players = await Player.getAll();
-        players.push(this);
-        await Player.saveAll(players);
-        resolve();
-      }catch(err){
-        reject(err);
+async function getbyId(id){
+  try{
+    let player = await prisma.player.findUnique({
+      where: {
+        userId: id
       }
+      return player
     })
+  }catch(err){
+    console.log(err);
   }
-  
-  static getById(id){
-    return new Promise(async (resolve, reject) => {
-      try{
-        const players = await Player.getAll();
-        const player = players.find(p => p.id === id);
-        resolve(player);
-      }catch(err){
-        reject(err);
-      }
-    })
-  }
-
-  static uptade(player){
-    return new Promise(async (resolve, reject) => {
-      try{
-        const players = await Player.getAll();
-        const index = players.findIndex(p => p.id === player.id);
-        players[index] = player;
-        await Player.saveAll(players);
-        resolve();
-      }catch(err){
-        reject(err);
-      }
-    })
-  }
-  
 }
-
-module.exports = Player;
+module.exports = {
+  getbyId
+}
